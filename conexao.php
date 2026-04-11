5e7ce8839bc4b1e3feb053f67ffe86a1bdbb082d
Raphael Melo <raphamelo41@gmail.com>
2026-04-10 22:07:11 -0300

    Banco rodando

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "SMCar");
+$conn = new mysqli("localhost", "root", "", "SMCar",3307);
 
 if ($conn->connect_error) {
     die("Erro: " . $conn->connect_error);
