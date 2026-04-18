5f7394dd756bee5223d9bcbdbccb1a84061ddf1d
AlessandroDias1979 <alessandroddigital@gmail.com>
2026-04-17 21:33:00 -0300

    adicionando o include

--- estoque.php
+++ estoque.php
@@ -1,56 +1,16 @@
 <?php
 include_once "./include/conexao.php";
 include_once "funcoes.php";
+include_once "./include/header.php";
 
 $marca = isset($_GET['marca']) ? $_GET['marca'] : "";
 
 $result = buscarVeiculos($conexao, $marca);
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Estoque de Veículos</title>
-    <link rel="stylesheet" href="estoque.css">
-</head>
-
-<body>
-
-    <!-- HEADER -->
-    <header class="topoHeader">
-        <div class="logoHeader">
-            <a href="index.html"><img src="/assests/logo2.png" SMCar></a>
-        </div>
-
-        <div class="buscaHeader">
-            <input type="text" placeholder="Buscar veículos...">
-            <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                    viewBox="0 0 24 24">
-                    <!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
-                    <path
-                        d="m17.06 14.94-2.8-1.34A6.96 6.96 0 0 0 16 9c0-3.86-3.14-7-7-7S2 5.14 2 9s3.14 7 7 7c1.76 0 3.37-.66 4.6-1.74l1.34 2.8 5 5 2.12-2.12zM9 14c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5">
-                    </path>
-                </svg></button>
-        </div>
-
-        <div class="redesHeader">
-            <a href="https://wa.me/5551994109429" target="_blank">
-                <img src="assests/pngtree-whatsapp-phone-icon-png-image_6315989.png" alt="WhatsApp"></a>
-
-            <a href="https://github.com/eisandromc" target="_blank">
-                <img src="assests/Instagram_icon.png" alt="Instagram"></a>
-        </div>
-    </header>
-    <!--NAV-->
-    <nav class="menuHeader">
-        <a href="index.html">HOME</a>
-        <a href="sobre.html">SOBRE</a>
-        <a href="estoque.html">ESTOQUE</a>
-        <a href="banco.html">BANCO ASSOCIADOS</a>
-        <a href="contato.html">CONTATOS</a>
-    </nav>
+<?php
+include_once "./include/header.php";
+?>
 
     <!-- MAIN -->
 
