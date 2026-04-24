82f595c8656dcca032fc48ac5ae85f1049aa9711
Henrique <henriquegbr2004@gmail.com>
2026-04-24 20:37:39 -0300

    pagina veiculo
    ajustado e funcional

--- veiculo.php
+++ veiculo.php
@@ -3,17 +3,27 @@ include 'include/conexao.php';
 include 'include/header.php';
 ?>
 
+<?php
+$id = $_GET['id'] ?? null;
+if (!$id || !is_numeric($id)) {
+    die("ID do veículo inválido ou não fornecido.");
+}
+?>
+
 <main>
     <?php
-    $sql = "SELECT * FROM Veiculos WHERE id_Veiculos = :id_Veiculos LIMIT 1 ";
-    $result = mysqli_query($conexao, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          echo '<img src="./assests/' . $row['Foto'] . '" alt="' . $row['Descricao'] . '">';
-        }
-    } else {
-        echo '<p>Nenhuma foto do veículo no momento.</p>';
-    }
+$sql = "SELECT * FROM Veiculos WHERE id_Veiculos = ? LIMIT 1";
+$stmt = mysqli_prepare($conexao, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id); // "i" para inteiro
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    echo '<img src="./assests/' . htmlspecialchars($row['Foto']) . '" alt="' . htmlspecialchars($row['Descricao']) . '">';
+} else {
+    echo '<p>Nenhuma foto do veículo no momento.</p>';
+}
+mysqli_stmt_close($stmt);
     ?>
 </main>
 
@@ -24,25 +34,24 @@ include 'include/header.php';
 <div>
 <p>
     <?php
-    $sql = "SELECT * FROM Veiculos WHERE id_Veiculos = :id_Veiculos";
-    $result = mysqli_query($conexao, $sql);
-    $veiculos = [];
-    while ($row = mysqli_fetch_assoc($result)) {
-    $veiculos[] = $row;
-    }
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          echo $veiculos[$i]['Ano'];
-          echo $veiculos[$i]['Kms']; 
-          echo $veiculos[$i]['Cambio']; 
-          echo $veiculos[$i]['Cor']; 
-          echo $veiculos[$i]['Carroceria']; 
-          echo number_format($veiculos[$i]['Preco'],0,",",".");;
-          echo $veiculos[$i]['Marca'];
-        }
-    } else {
-        echo '<p>Nenhuma informação sobre o veículo no momento.</p>';
-    }
+$sql = "SELECT * FROM Veiculos WHERE id_Veiculos = ? LIMIT 1";
+$stmt = mysqli_prepare($conexao, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    echo '<p>Ano: ' . htmlspecialchars($row['Ano']) . '<br>';
+    echo 'Kms: ' . htmlspecialchars($row['Kms']) . '<br>';
+    echo 'Câmbio: ' . htmlspecialchars($row['Cambio']) . '<br>';
+    echo 'Cor: ' . htmlspecialchars($row['Cor']) . '<br>';
+    echo 'Carroceria: ' . htmlspecialchars($row['Carroceria']) . '<br>';
+    echo 'Preço: R$ ' . number_format($row['Preco'], 0, ',', '.') . '<br>';
+    echo 'Marca: ' . htmlspecialchars($row['Marca']) . '</p>';
+} else {
+    echo '<p>Nenhuma informação sobre o veículo no momento.</p>';
+}
+mysqli_stmt_close($stmt);
     ?>
 </p>
 </div>
