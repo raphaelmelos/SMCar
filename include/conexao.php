f582e4d5c227c845b6055cc25b382f314052eb57
AlessandroDias1979 <alessandroddigital@gmail.com>
2026-04-24 20:40:55 -0300

    comit

--- include/conexao.php
+++ include/conexao.php
@@ -5,9 +5,9 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'SmCar');
+define('DB_NAME', 'SMCar');
 
-$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3307);
+$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
 
 if (!$conexao) {
     die("Falha na conexão: " . mysqli_connect_error());
