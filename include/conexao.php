8cb5c14da6126e3ff66299649225c2a10f42cd9b
Henrique <henriquegbr2004@gmail.com>
2026-04-24 21:12:36 -0300

    port
    3307

--- include/conexao.php
+++ include/conexao.php
@@ -7,7 +7,7 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'SMCar');
 
-$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
+$conexao = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3307);
 
 if (!$conexao) {
     die("Falha na conexão: " . mysqli_connect_error());
